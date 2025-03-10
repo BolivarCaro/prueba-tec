a3110e16210e4fbc78aff9e3a2e6cb95349309c6
rootEl <11406509+BolivarCaro@users.noreply.github.com>
2025-03-10 10:47:18 -0500

    feat: modificacion del la clase Models, eliminacion de la clase message, ahora la clase message y user en el archivo models

--- prueba-tec-env/app/models/message.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
-
-class Message(SQLModel, Table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: str
-    question : str
-    response: str
---
+++ prueba-tec-env/app/models/models.py
@@ -0,0 +1,22 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+
+class User(SQLModel, table = True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, index=True)
+    role: str
+    #relacion con los mensajes (mensajes relacionados con el usuario)
+    messages: List["Message"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={
+            "cascade": "all, delete"
+        }
+    )
+
+
+class Message(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    question : str
+    response: str
+    #realcion con la clase User, permite acceder al usuario desde un mensaje
+    user: Optional[User] = Relationship(back_populates="messages")
